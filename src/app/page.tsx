df901f1136873eb61c24af912590ca574948e0d6
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-01 00:06:53 -0500

    Fix some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
     <div className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" style={{background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`}}></div>
-    <main className="mx-auto px-6 py-12 font-sans flex min-h-screen flex-col md:flex-row bg-slate-900 text-slate-400 md:justify-evenly md:px-12 md:py-20 lg:px-24 lg:h-screen lg:w-screen lg:overflow-hidden" >
+    <main className="mx-auto px-6 py-12 font-sans flex min-h-screen flex-col md:flex-row bg-slate-900 text-slate-400 md:justify-evenly md:px-12 md:pt-20 md:pb-2 lg:px-24 lg:h-screen lg:w-screen lg:overflow-hidden" >
       <div className="flex flex-col lg:flex-row lg:justify-between gap-4 max-w-[1440px]">
         <Header />
         <div className="md:w-1/2 lg:overflow-y-scroll scroll-smooth">
