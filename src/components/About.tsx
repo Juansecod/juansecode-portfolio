e2dab65f7150f7a56f259378410fbb4e917ad479
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-02 21:49:16 -0500

    Add links to view more info about my skills

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,61 +5,75 @@ import { faGithub, faJira, faSlack, faJava, faJs, faHtml5, faCss3, faGit, faNode
 const TECH_KNOWLEDGE = [{
     name: "html",
     icon: faHtml5,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://developer.mozilla.org/docs/Web/HTML"
 },{
     name: "css",
     icon: faCss3,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://developer.mozilla.org/docs/Web/CSS"
 },{
     name: "javascript",
     icon: faJs,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://developer.mozilla.org/docs/Web/JavaScript"
 },{
     name: "node.js",
     icon: faNodeJs,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://nodejs.org/"
 }, {
     name: "express",
     icon: faUncharted,
-    haveIcon: false
+    haveIcon: false,
+    link: "https://expressjs.com/"
 }, {
     name: "react",
     icon: faReact,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://react.dev/"
 },{
     name: "python",
     icon: faPython,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://www.python.org/"
 },{
     name: "java",
     icon: faJava,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://www.java.com/"
 },{
-    name: "Spring Boot",
+    name: "Spring",
     icon: faUncharted,
-    haveIcon: false
+    haveIcon: false,
+    link: "https://spring.io/"
 },{
     name: "MySQL",
     icon: faDatabase,
-    haveIcon: false
+    haveIcon: false,
+    link: "https://www.mysql.com/"
 },{
     name: "git",
     icon: faGit,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://git-scm.com/"
 }]
 
 const TEAM_KNOWLEDGE = [{
     name: "jira",
     icon: faJira,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://www.atlassian.com/software/jira"
 },{
     name: "slack",
     icon: faSlack,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://slack.com/"
 },{
     name: "github",
     icon: faGithub,
-    haveIcon: true
+    haveIcon: true,
+    link: "https://github.com/"
 }]
 
 const About = () => {
@@ -71,20 +85,31 @@ const About = () => {
             {"In the following sections, you'll find an overview of my technical skills and team management abilities, providing insights into my expertise in software development and technology."}
             <h5 className="text-white bg-slate-900 font-semibold text-md text-center py-2">Technical skills</h5>
             <div className="flex flex-row gap-3 px-5 justify-evenly flex-wrap">
-                {TECH_KNOWLEDGE.map(({name, icon, haveIcon}, index)=>
-                    (<i className={`lg:hover:bg-slate-950 flex flex-col justify-center items-center  max-w-[50px] max-h-[50px] py-2 rounded-md md:max-w-[75px] md:max-h-[75px] xl:max-w-[100px] xl:max-h-[100px] hover:text-slate-200 cursor-pointer text-center` + (!haveIcon ? ` hidden xl:block` : ``)} key={index} title={name.charAt(0).toUpperCase() + name.slice(1)}>
+                {TECH_KNOWLEDGE.map(({name, icon, haveIcon, link}, index)=>
+                    (<a href={link} 
+                        target="_blank" 
+                        rel="noreferrer noopener" 
+                        aria-label={`${name} documentation (opens in a new tab)`} 
+                        className={`lg:hover:bg-slate-950 flex flex-col justify-center items-center  max-w-[50px] max-h-[50px] py-2 rounded-md md:max-w-[75px] md:max-h-[75px] xl:max-w-[100px] xl:max-h-[100px] hover:text-slate-200 cursor-pointer text-center` + (!haveIcon ? ` hidden xl:block` : ``)} 
+                        key={index} title={name.charAt(0).toUpperCase() + name.slice(1)}
+                    >
                         <FontAwesomeIcon className="w-[80%] h-[80%] hover:transform hover:scale-125 xl:hover:scale-110 transition-all" icon={icon} />
                         <i className="hidden w-100% xl:block">{name.charAt(0).toUpperCase() + name.slice(1)}</i>
-                    </i>)
+                    </a>)
                 )}
             </div>
             <h5 className="text-white bg-slate-900 font-semibold text-md text-center py-2">Team Management</h5>
             <div className="flex flex-row gap-3 px-5 justify-evenly flex-wrap">
-                {TEAM_KNOWLEDGE.map(({name, icon, haveIcon}, index)=>
-                    (<i className={`lg:hover:bg-slate-950 flex flex-col justify-center items-center  max-w-[50px] max-h-[50px] py-2 rounded-md md:max-w-[75px] md:max-h-[75px] xl:max-w-[100px] xl:max-h-[100px] hover:text-slate-200 cursor-pointer text-center` + (!haveIcon ? ` hidden xl:block` : ``)} key={index} title={name.charAt(0).toUpperCase() + name.slice(1)}>
+                {TEAM_KNOWLEDGE.map(({name, icon, haveIcon, link}, index)=>
+                    (<a href={link} 
+                        target="_blank" 
+                        rel="noreferrer noopener" 
+                        aria-label={`${name} documentation (opens in a new tab)`} 
+                        className={`lg:hover:bg-slate-950 flex flex-col justify-center items-center  max-w-[50px] max-h-[50px] py-2 rounded-md md:max-w-[75px] md:max-h-[75px] xl:max-w-[100px] xl:max-h-[100px] hover:text-slate-200 cursor-pointer text-center` + (!haveIcon ? ` hidden xl:block` : ``)} 
+                        key={index} title={name.charAt(0).toUpperCase() + name.slice(1)}>
                         <FontAwesomeIcon className="w-[80%] h-[80%] hover:transform hover:scale-125 xl:hover:scale-110 transition-all" icon={icon} />
                         <i className="hidden w-100% xl:block">{name.charAt(0).toUpperCase() + name.slice(1)}</i>
-                    </i>)
+                    </a>)
                 )}
             </div>
         </section>
