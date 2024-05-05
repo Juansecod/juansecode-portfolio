4a6da3bb269f08c37349f9f68c044579921628b6
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-05 17:09:20 -0500

    Fix style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -72,7 +72,7 @@ const Header: React.FC<HeaderProps> = ({activePage, setActivePage}) => {
                     <ul className="mt-5 w-max">
                         {PAGES.map(({page})=>(
                             <Link className={"group flex items-center py-3 active"} href={`#${page}`} key={page} onClick={(e)=>{setActivePage(page)}}>
-                                <span className={"nav-indicator mr-4 h-px w-8  transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none " + (activePage.includes(page) ? "bg-slate-200 w-16" : "bg-slate-600")}></span>
+                                <span className={"nav-indicator mr-4 h-px transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none " + (activePage.includes(page) ? "bg-slate-200 w-16" : "bg-slate-600 w-8")}></span>
                                 <span className={"nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200 "+ (activePage.includes(page) ? "text-slate-200" : "text-slate-500 ")}>{page}</span>
                             </Link>
                         ))}
