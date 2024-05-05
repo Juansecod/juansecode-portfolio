646fc9fbf238eb82157ed5131351ca364876424f
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-05 15:10:04 -0500

    Fix problem path in a pic in projects section

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -28,9 +28,6 @@ const ProjectItem = ({item}:{item:ProjectItemInterface}) => {
 
     return (
         <li className="flex flex-col lg:flex-row gap-2 p-2 lg:p-4 transition-all sm:gap-8 md:gap-4 lg:hover:bg-slate-950 lg:hover:bg-opacity-50  rounded">
-            <header className="mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2 lg:max-w-[20%] self-center">
-                <Image src={image} alt={"Preview " + name} title={"Preview " + name} className="rounded lg:hover:transition-all lg:hover:transform lg:hover:scale-110 cursor-pointer" onClick={handleModal} />
-            </header>
             <div className="lg:max-w-[80%]">
                 <div className="text-slate-200 hover:text-teal-300 focus-visible:text-teal-300" >
                     <a href={link} target="_blank" rel="noreferrer noopener">
@@ -58,13 +55,18 @@ const ProjectItem = ({item}:{item:ProjectItemInterface}) => {
                 </ul>
             </div>
             {modalDisplay && 
-                (<div className="w-screen h-screen bg-slate-950 bg-opacity-50 fixed top-0 left-0 z-50 px-2 lg:py-20 lg:px-20 flex items-center justify-center flex-col gap-2" onClick={handleModal}>
-                    <i className="self-end text-teal-200 hidden hover:text-cyan-500 hover:transform hover:scale-125 w-5 h-5  items-center justify-center rounded mx-5 cursor-pointer lg:flex">
-                        <FontAwesomeIcon icon={faClose} onClick={handleModal}/>
-                    </i>
-                    <Image src={image} alt={"Preview " + name} title={"Preview " + name} className="rounded lg:w-full lg:h-full" onClick={stopPropagation} />
+                (<div className="w-screen h-screen bg-slate-950 bg-opacity-50 fixed top-0 left-0 z-50 px-2 lg:py-20 lg:px-20 flex items-center justify-center flex-col lg:pb-5" onClick={handleModal}>
+                    <div className="max-w-[1440px] flex items-center justify-center flex-col gap-2">
+                        <i className="self-end text-teal-200 hidden hover:text-cyan-500 hover:transform hover:scale-125 w-5 h-5  items-center justify-center rounded cursor-pointer lg:flex relative right-10">
+                            <FontAwesomeIcon icon={faClose} onClick={handleModal}/>
+                        </i>
+                        <Image src={image} alt={"Preview " + name} title={"Preview " + name} className="rounded lg:h-full lg:w-auto aspect-video" onClick={stopPropagation} />
+                    </div>
                 </div>)
             }
+            <footer className="mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2 lg:max-w-[20%] self-center">
+                <Image src={image} alt={"Preview " + name} title={"Preview " + name} className="rounded lg:hover:transition-all lg:hover:transform lg:hover:scale-110 cursor-pointer" onClick={handleModal} />
+            </footer>
         </li>
     )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import ProjectItem from "./ProjectItem";
-import sidesImg from "@/assets/Sides.png";
+import sidesImg from "@/assets/sides.png";
 
 const PROJECTS = [{
     name: "SIDES",
@@ -8,7 +8,7 @@ const PROJECTS = [{
     description: "SIDES (Software Engineering Seminar) is an event organized by students and the Software Engineering coordination of the Faculty of Engineering at the Cooperative University of Colombia, Medellín Campus. It covers essential topics in software engineering, catering to students, professionals, and the community interested in software engineering activities, competencies, and challenges, with a focus on future trends and potential specializations such as databases, artificial intelligence, and cybersecurity. The seminar also features talks by experts in related fields, providing valuable insights for personal, professional, and social development of attendees.",
     technologies: ["PHP","HTML","CSS","Bootstrap","SQL","Git","GitHub"],
     repo: "https://github.com/AlejandroV074/SIDES-P"
-}]
+}];
 
 const Projects = ()=>{
     return (
